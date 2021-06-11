2db81a5ceb43ad91256fd87f5027afdee3826513
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-06-11 14:44:04 +0200

    removes old idea to pass arguments

--- src/extension.ts
+++ src/extension.ts
@@ -24,10 +24,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mope-client.connect', (port) => {
+	let disposable = vscode.commands.registerCommand('mope-client.connect', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		console.log(port);
 		let options: vscode.InputBoxOptions = {
 			prompt: "Server port:"
 		};
